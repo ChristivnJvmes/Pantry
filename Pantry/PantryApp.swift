b90a6c4b039fc5f6877247139744c4f07f23f5b5
Christian Wilson <87831034+ChristivnJvmes@users.noreply.github.com>
2022-12-02 09:33:51 -0600

    Add files via upload

---
+++ Pantry/PantryApp.swift
@@ -0,0 +1,18 @@
+//
+//  PantryApp.swift
+//  Pantry
+//
+//  Created by Christian Wilson on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
+
